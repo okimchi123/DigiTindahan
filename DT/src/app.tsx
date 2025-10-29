db99f4f6e54a646244129b006f6b1b140065f2db
Okimchi <basilio.miko123@gmail.com>
2025-10-30 02:40:48 +0800

    updated the routing

--- DT/src/app.tsx
+++ DT/src/app.tsx
@@ -1,11 +1,28 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import LandingPage from './Pages/Landing/Page';
+import RootPage from './Pages/Root/RootPage';
+import NotFoundPage  from './Pages/NotFound/Page';
+import Dashboard from './Pages/Dashboard/Page';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <LandingPage />,
   },
+  {
+    element: <RootPage />,
+    //change this element as protectedRoute in the future
+    children: [
+      {
+        path: '/Dashboard',
+        element: <Dashboard />,
+      },
+    ]
+  },
+  {
+    path: '*',
+    element: <NotFoundPage />
+  }
 ]);
 
 function App(){
