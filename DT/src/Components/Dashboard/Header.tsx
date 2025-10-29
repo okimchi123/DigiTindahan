e0bef15f17aaae71955300ce0978685760355813
Okimchi <basilio.miko123@gmail.com>
2025-10-30 02:42:06 +0800

    created header component for dashboard

---
+++ DT/src/Components/Dashboard/Header.tsx
@@ -0,0 +1,10 @@
+
+export default function DashHeader () {
+  return (
+    <header className="flex flex-col items-center py-8">
+        <h3 className="font-semibold">Good Morning</h3>
+        <h1 className="text-[36px] leading-8 font-bold">Mom</h1>
+        <p className="font-bold text-secondary">I hope you're having a good day</p>
+    </header>
+  );
+}
