ad4fc35441572985eed222b5ab377da95577b19d
Okimchi <basilio.miko123@gmail.com>
2025-10-30 02:41:01 +0800

    initialized the rootpage design

--- DT/src/Pages/Root/RootPage.tsx
+++ DT/src/Pages/Root/RootPage.tsx
@@ -1,9 +1,15 @@
 import { Outlet } from "react-router-dom";
 
-export function RootPage () {
+export default function RootPage() {
   return (
     <main className="w-full h-full relative flex flex-col items-center justify-start">
-        <Outlet />
+      <nav className="w-[85%] py-4">
+        <h1 className="text-[18px] font-bold">
+          <span className="text-primary">D</span>igi
+          <span className="text-primary">T</span>indahan
+        </h1>
+      </nav>
+      <Outlet />
     </main>
   );
 }
