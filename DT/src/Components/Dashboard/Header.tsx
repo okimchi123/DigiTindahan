2620a60c341cda0c5b25dc5e0759ceece7a5507e
Okimchi <basilio.miko123@gmail.com>
2025-11-01 00:13:17 +0800

    added animation to dashboard

--- DT/src/Components/Dashboard/Header.tsx
+++ DT/src/Components/Dashboard/Header.tsx
@@ -1,10 +1,46 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 
-export default function DashHeader () {
+export default function DashHeader() {
+  useGSAP(() => {
+    const tl = gsap.timeline();
+
+    tl.from("#text1", {
+      duration: 0.5,
+      y: 40,
+      autoAlpha:0,
+    })
+      .from(
+        "#text2",
+        {
+          duration: 0.5,
+          y: 40,
+          autoAlpha:0,
+        },
+        "-=0.2"
+      )
+      .from(
+        "#text3",
+        {
+          duration: 0.5,
+          y: 40,
+          autoAlpha:0,
+        },
+        "-=0.2"
+      );
+  }, []);
+
   return (
-    <header className="flex flex-col items-center py-8">
-        <h3 className="font-semibold">Good Morning</h3>
-        <h1 className="text-[36px] leading-8 font-bold">Mom</h1>
-        <p className="font-bold text-secondary">I hope you're having a good day</p>
+    <header className="flex flex-col items-center py-8 ">
+      <h3 id="text1" className="font-semibold">
+        Good Morning
+      </h3>
+      <h1 id="text2" className="text-[36px] leading-8 font-bold">
+        Mom
+      </h1>
+      <p id="text3" className="font-bold text-secondary">
+        I hope you're having a good day
+      </p>
     </header>
   );
 }
