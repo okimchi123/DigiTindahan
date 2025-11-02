3b15e4475a4716e446ebe24961d489adeac200f9
Okimchi <basilio.miko123@gmail.com>
2025-11-02 23:53:35 +0800

    finilized dashboard in and out animation

--- DT/src/Pages/Dashboard/Page.tsx
+++ DT/src/Pages/Dashboard/Page.tsx
@@ -1,17 +1,50 @@
-import { Link } from "react-router-dom";
 import DashHeader from "../../Components/Dashboard/Header";
 import Actions from "../../Model/Actions";
 import { ChevronRight } from "lucide-react";
+import gsap from "gsap";
+import { useNavigate, useLocation } from "react-router-dom";
+import { useGSAP } from "@gsap/react";
 
 export default function Dashboard () {
+  const navigate = useNavigate();
+  const location = useLocation();
+  
+  const handleRoute = (e: string) => {
+    gsap.to('#dashCont', {
+      x:'-100%',
+      duration:0.3,
+      ease: 'power2.inOut',
+      onComplete: ()=>{
+        navigate(e);
+      },
+    });
+  }
+
+  useGSAP(() => {
+    const fromBack = location.state?.fromBack;
+    
+    gsap.fromTo('#dashCont',
+      { x: fromBack ? '-100%' : '0%' },
+      { 
+        x: 0,
+        duration: 0.3,
+        ease: 'power2.inOut',
+        onComplete: () => {
+          navigate(location.pathname, { state: {} });
+        },
+      }
+    );
+
+  }, []);
+
   return (
-    <div className="w-full h-full flex flex-col items-center">
+    <div id="dashCont" className="container">
       <DashHeader />
       <section className="w-[83%] mt-2">
         <h4 className="font-semibold">Choose your Actions</h4>
         <div className="flex flex-col gap-3">
             {Actions.map((item)=>(
-                <Link key={`${item.title}`} to={`${item.route}`}>
+                <button onClick={()=>handleRoute(`${item.route}`)} key={`${item.title}`}>
                     <div className="relative flex gap-2 rounded-lg items-center bg-greenCard py-4 pl-2">
                         <div className="p-2 bg-white/40 rounded-lg"><item.icon size='32' color="white" /></div>
                         <div className="flex flex-col gap-1">
@@ -20,7 +53,7 @@ export default function Dashboard () {
                         </div>
                         <ChevronRight size='32' color='white' className="absolute right-1" />
                     </div>
-                </Link>
+                </button>
             ))}
         </div>
       </section>
