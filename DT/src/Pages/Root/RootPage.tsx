2ad6bcb9a880bb86a6c82962523c03fa7bded6f5
Okimchi <basilio.miko123@gmail.com>
2025-11-02 23:53:53 +0800

    added a back animation in rootPage

--- DT/src/Pages/Root/RootPage.tsx
+++ DT/src/Pages/Root/RootPage.tsx
@@ -1,10 +1,30 @@
 import { Outlet, useLocation, Link } from "react-router-dom";
 import { ChevronLeft } from "lucide-react";
 import clsx from "clsx";
+import { useNavigate } from "react-router-dom";
+import gsap from "gsap";
 
 export default function RootPage() {
   const location = useLocation();
   const {pathname} = location;
+  const navigate = useNavigate();
+
+  const handleBack = () => {
+    const targetElement = document.querySelector('#cont');
+
+    if (targetElement) {
+      gsap.to(targetElement, {
+        x: '100%',
+        duration: 0.2,
+        ease: 'power2.inOut',
+        onComplete: () => {
+          navigate('/dashboard', { state: { fromBack: true } });
+        }
+      });
+    } else {
+      navigate('/dashboard', { state: { fromBack: true } });
+    }
+  };
 
   return (
     <main className="w-full h-full relative flex flex-col items-center justify-start">
@@ -13,7 +33,7 @@ export default function RootPage() {
         "w-[83%]":pathname === '/dashboard',
       })}>
         <div className="flex items-center">
-          {pathname !== '/dashboard' && <Link to='/dashboard'><ChevronLeft size='31'/></Link>}
+          {pathname !== '/dashboard' && <button onClick={handleBack}><ChevronLeft size='31'/></button>}
         <h1 className={`text-[18px] font-bold ${pathname !== '/dashboard' ? "mb-0.5":"mt-px"}`}>
           <span className="text-primary">D</span>igi
           <span className="text-primary">T</span>indahan
