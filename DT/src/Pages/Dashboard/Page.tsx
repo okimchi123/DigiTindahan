ce432c6ed7ec4df09f0e8dc95e2eaa3861cd8139
Okimchi <basilio.miko123@gmail.com>
2025-11-02 23:57:24 +0800

    fixed actions ui in dashboard

--- DT/src/Pages/Dashboard/Page.tsx
+++ DT/src/Pages/Dashboard/Page.tsx
@@ -47,7 +47,7 @@ export default function Dashboard () {
                 <button onClick={()=>handleRoute(`${item.route}`)} key={`${item.title}`}>
                     <div className="relative flex gap-2 rounded-lg items-center bg-greenCard py-4 pl-2">
                         <div className="p-2 bg-white/40 rounded-lg"><item.icon size='32' color="white" /></div>
-                        <div className="flex flex-col gap-1">
+                        <div className="flex flex-col items-start gap-1">
                             <h3 className="text-white font-bold text-[18px] leading-5">{item.title}</h3>
                             <p className="text-[#E0FFE7] text-sm font-semibold leading-4">{item.subtitle}</p>
                         </div>
