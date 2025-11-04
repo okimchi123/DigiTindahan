188f37710dded12eb51c0666387c22e6f0782634
Okimchi <basilio.miko123@gmail.com>
2025-11-04 20:01:15 +0800

    integrated the calcu component to the calcu page

--- DT/src/Pages/CalculateProfit/Page.tsx
+++ DT/src/Pages/CalculateProfit/Page.tsx
@@ -2,6 +2,7 @@ import { useGSAP } from "@gsap/react";
 import clsx from "clsx";
 import gsap from "gsap";
 import { useState, useEffect } from "react";
+import Calculate from "../../Components/Calcu-Profit/Calculate";
 
 interface formProp {
   POR: number;
@@ -19,133 +20,59 @@ export default function CalculateProfit() {
   const [profit, setProfit] = useState("");
   const [total, setTotal] = useState(0);
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target;
-    setForm((prev) => ({
-      ...prev,
-      [name]: value,
-    }));
-
-    if (form.POR && form.QTY && form.SP && form.PP) {
-      const newTotal = Math.floor((form.QTY / form.PP) * form.SP - form.POR);
-      setTotal(newTotal);
-    }
-  };
-
-  const Clear = () => {
-    setTotal(0);
-    setProfit("noProfit")
-    setForm({
-      POR: 0,
-      QTY: 0,
-      SP: 0,
-      PP: 0,
-    });
-  };
-
   useEffect(() => {
     if (form.POR && form.QTY && form.SP && form.PP) {
       const newTotal = Math.floor((form.QTY / form.PP) * form.SP - form.POR);
       setTotal(newTotal);
-      if(newTotal>0){
+      if (newTotal > 0) {
         setProfit("win");
-      }else if(newTotal<0){
+      } else if (newTotal < 0) {
         setProfit("lose");
-      }else if(newTotal===0){
-        setProfit("noProfit")
+      } else if (newTotal === 0) {
+        setProfit("noProfit");
       }
     }
   }, [form.POR, form.QTY, form.SP, form.PP]);
 
-  useGSAP(()=>{
-    gsap.fromTo('#cont',
-      { x: '100%' },
-      { 
+  useGSAP(() => {
+    gsap.fromTo(
+      "#cont",
+      { x: "100%" },
+      {
         x: 0,
         duration: 0.2,
-        ease: 'power2.inOut'
-      });
-  },[])
+        ease: "power2.inOut",
+      }
+    );
+  }, []);
 
   return (
     <div id="cont" className="container">
       <header className="header-cont">
         <h1 className="header-font">Calculate Profit</h1>
-        <h2 className={clsx("text-[36px] text-gray",{
-            "text-light-green":profit==='win',
-            "text-red-500":profit==='lose',
-            "text-gray":profit==='noProfit',
-          })}>
+        <h2
+          className={clsx("text-[36px] text-gray", {
+            "text-light-green": profit === "win",
+            "text-red-500": profit === "lose",
+            "text-gray": profit === "noProfit",
+          })}
+        >
           ₱<span className="header-font">{total}</span>
         </h2>
-        <p className="font-semibold text-[18px]">{profit==='win' ? "Made Profit" : profit==='lose' ? "Loss Made" : "No Profit"}</p>
+        <p className="font-semibold text-[18px]">
+          {profit === "win"
+            ? "Made Profit"
+            : profit === "lose"
+            ? "Loss Made"
+            : "No Profit"}
+        </p>
       </header>
-      <form className="flex-col-center py-1 gap-3">
-        <div className="input-label-cont">
-          <label className="font-semibold text-[18px]" htmlFor="ogprice">
-            Product Original Price
-          </label>
-          <input
-            type="number"
-            onChange={handleChange}
-            name="POR"
-            value={form.POR}
-            id="ogprice"
-            className="input-design"
-            placeholder="0"
-            min="0"
-          />
-        </div>
-        <div className="input-label-cont">
-          <label className="font-semibold text-[18px]" htmlFor="qnt">
-            Quantity
-          </label>
-          <input
-            type="number"
-            onChange={handleChange}
-            name="QTY"
-            value={form.QTY}
-            id="qnt"
-            className="input-design"
-            placeholder="0"
-          />
-        </div>
-        <div className="input-label-cont">
-          <label className="font-semibold text-[18px]" htmlFor="sp">
-            Selling Price
-          </label>
-          <input
-            type="number"
-            onChange={handleChange}
-            name="SP"
-            value={form.SP}
-            id="sp"
-            className="input-design"
-            placeholder="0"
-          />
-        </div>
-        <div className="input-label-cont">
-          <label className="font-semibold text-[18px]" htmlFor="pp">
-            Per Piece
-          </label>
-          <input
-            type="number"
-            onChange={handleChange}
-            name="PP"
-            value={form.PP}
-            id="pp"
-            className="input-design"
-            placeholder="0"
-          />
-        </div>
-        <button
-          onClick={Clear}
-          type="button"
-          className="font-bold text-[18px] text-gray p-2 mt-5"
-        >
-          Clear all
-        </button>
-      </form>
+      <Calculate
+        form={form}
+        setForm={setForm}
+        setTotal={setTotal}
+        setProfit={setProfit}
+      />
     </div>
   );
 }
