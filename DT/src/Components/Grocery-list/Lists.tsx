f2cce87406f16f497ff5faf37d90b4fa4864b40e
Okimchi <basilio.miko123@gmail.com>
2025-11-06 03:15:27 +0800

    added interactivity to Lists

--- DT/src/Components/Grocery-list/Lists.tsx
+++ DT/src/Components/Grocery-list/Lists.tsx
@@ -1,25 +1,46 @@
 import CustomSearch from "../UI/Search";
+import GroceryItem from "./GroceryItem";
+import { useState } from "react";
+import { createPortal } from "react-dom";
+
+interface props {
+  Date: string;
+  Preview: string;
+}
+const List: props[] = [
+    {
+      Date: "Sunday, October 26, 2025",
+      Preview: "Piatos blue - 10",
+    },
+    {
+      Date: "Wed, October 15, 2025",
+      Preview: "555 green - 8",
+    },
+  ];
 
 export default function Lists() {
+  const [modal, setModal] = useState(false);
 
-  
   return (
-    <section className="flex flex-col gap-2 w-[80%]">
-      <CustomSearch />
-      <ul className="flex flex-col gap-4">
-        <li>
-          <button aria-haspopup="dialog" className="bg-input w-full flex flex-col items-start py-4 pl-2 rounded-xl">
-            <h2 className="font-bold text-xl">Sunday, October 26, 2025</h2>
-            <p className="font-semibold text-gray text-lg">Piatos blue - 10</p>
-          </button>
-        </li>
-        <li>
-          <button aria-haspopup="dialog" className="bg-input w-full flex flex-col items-start py-4 pl-2 rounded-xl">
-            <h2 className="font-bold text-xl">Wed, October 15, 2025</h2>
-            <p className="font-semibold text-gray text-lg">555 green - 8</p>
-          </button>
-        </li>
-      </ul>
-    </section>
+    <>
+    { modal && createPortal(<GroceryItem onClose={()=>setModal(false)} />, document.getElementById('mainPage')!)}
+      <section className="flex flex-col gap-2 w-[80%]">
+        <CustomSearch />
+        <ul className="flex flex-col gap-4">
+          {List.map((i) => (
+            <li key={i.Date}>
+              <button
+              onClick={()=>setModal(true)}
+                aria-haspopup="dialog"
+                className="bg-input w-full flex flex-col items-start py-4 pl-2 rounded-xl"
+              >
+                <h2 className="font-bold text-xl">{i.Date}</h2>
+                <p className="font-semibold text-gray text-lg">{i.Preview}</p>
+              </button>
+            </li>
+          ))}
+        </ul>
+      </section>
+    </>
   );
 }
