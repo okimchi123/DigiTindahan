1e4e39989fc5d34610798aabbc35a3d3fe0f600b
Okimchi <basilio.miko123@gmail.com>
2025-11-10 03:25:48 +0800

    connected controller to auth route and added cors to app

--- backend/app.ts
+++ backend/app.ts
@@ -1,11 +1,19 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import AuthRoutes from './routes/auth.route';
+import cors from 'cors';
 
 const app = express();
 
 dotenv.config();
 
+app.use(cors({
+    origin: process.env.ORIGIN,
+    methods:['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true,
+}))
+
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.get('/', (req, res) => {
--- backend/routes/auth.route.ts
+++ backend/routes/auth.route.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import pool from '../database/connection';
+import { RegisterUser } from '../Controller/Auth.controller';
 
 const router = express.Router();
 
@@ -8,8 +9,9 @@ router.get('/', async (req, res) => {
       const [rows] = await pool.query("SELECT * from user");
       res.status(200).json(rows);
     } catch (error) {
-        res.status(500).json({ message: error });
+      res.status(500).json({ message: error });
     }
 });
 
+router.post('/register', RegisterUser);
 export default router;
