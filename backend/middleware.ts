396c36072cb8517ba02e9fbd265801c0fd5b734d
Okimchi <basilio.miko123@gmail.com>
2025-11-13 02:48:14 +0800

    created a middleware for backend

---
+++ backend/middleware.ts
@@ -0,0 +1,31 @@
+import { Request, Response, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
+
+
+export interface AuthRequest extends Request {
+  user?: {
+    userId: number;
+  };
+}
+
+export const authenticateToken = (
+  req: AuthRequest,
+  res: Response,
+  next: NextFunction
+) => {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+
+  if (!token) {
+    return res.status(401).json({ message: 'Access token required' });
+  }
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET!, (err: any, decoded: any) => {
+    if (err) {
+      return res.status(403).json({ message: 'Invalid or expired token' });
+    }
+
+    req.user = decoded;
+    next();
+  });
+};
