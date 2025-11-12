3ba42e8ee96a09ba9ee6a2bf2092c9085616980c
Okimchi <basilio.miko123@gmail.com>
2025-11-13 04:27:49 +0800

    created fetch grocerylist api

---
+++ DT/src/Hooks/GroceryListAPI/FetchGrocery.ts
@@ -0,0 +1,26 @@
+import { useQuery } from '@tanstack/react-query';
+import { axiosInstance } from '../../Model/AxiosInstance';
+
+export interface GroceryList {
+  list_id: number;
+  user_id: number;
+  list_name: string;
+  latest_item: string;
+  latest_item_quantity: number;
+  created_at: string;
+  updated_at: string;
+}
+
+const getAllLists = async (): Promise<GroceryList[]> => {
+    const { data } = await axiosInstance.get('/grocery/');
+    return data;
+  }
+
+const useGroceryLists = () => {
+  return useQuery({
+    queryKey: ['grocery-lists'],
+    queryFn: getAllLists,
+  });
+};
+
+export default useGroceryLists;
