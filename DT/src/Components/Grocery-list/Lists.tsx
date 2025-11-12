e7285a175dbf8ad2673c7688fe8c298731078d34
Okimchi <basilio.miko123@gmail.com>
2025-11-13 04:28:10 +0800

    connected the fetch allgrocerylist api in frontend

--- DT/src/Components/Grocery-list/Lists.tsx
+++ DT/src/Components/Grocery-list/Lists.tsx
@@ -2,44 +2,41 @@ import CustomSearch from "../UI/Search";
 import GroceryItem from "./GroceryItem";
 import { useState } from "react";
 import { createPortal } from "react-dom";
-
-interface props {
-  Date: string;
-  Preview: string;
-}
-const List: props[] = [
-    {
-      Date: "Sunday, October 26, 2025",
-      Preview: "Piatos blue - 10",
-    },
-    {
-      Date: "Wed, October 15, 2025",
-      Preview: "555 green - 8",
-    },
-  ];
+import useGroceryLists from "../../Hooks/GroceryListAPI/FetchGrocery";
+import dayjs from "dayjs";
 
 export default function Lists() {
   const [modal, setModal] = useState(false);
+  const [selectedList, setSelectedList] = useState<number | null>(null);
+  const { data, isLoading, isError } = useGroceryLists();
+
+  const onSelect = (i: number) => {
+  setSelectedList(i)
+  setModal(true)
+  }
 
   return (
     <>
-    { modal && createPortal(<GroceryItem onClose={()=>setModal(false)} />, document.getElementById('mainPage')!)}
+    { modal && createPortal(<GroceryItem onClose={()=>setModal(false)} listId={selectedList} />, document.getElementById('mainPage')!)}
       <section className="flex flex-col gap-2 w-[80%]">
         <CustomSearch />
-        <ul className="flex flex-col gap-4">
-          {List.map((i) => (
-            <li key={i.Date}>
+        {data?.length && (
+          <ul className="flex flex-col gap-4">
+          {data.map((i) => (
+            <li key={i.created_at}>
               <button
-              onClick={()=>setModal(true)}
+              onClick={()=>onSelect(i.list_id)}
                 aria-haspopup="dialog"
                 className="bg-input w-full flex flex-col items-start py-4 pl-2 rounded-xl"
               >
-                <h2 className="font-bold text-xl">{i.Date}</h2>
-                <p className="font-semibold text-gray text-lg">{i.Preview}</p>
+                <h2 className="font-bold text-xl">{dayjs(i.list_name).format('MMMM D, YYYY')}</h2>
+                <p className="font-semibold text-gray text-lg">{i.latest_item} - {i.latest_item_quantity}</p>
               </button>
             </li>
           ))}
         </ul>
+        )}
+        
       </section>
     </>
   );
