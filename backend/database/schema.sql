c7141bad5233585c2511ffab1e4d0c1b69c82397
Okimchi <basilio.miko123@gmail.com>
2025-11-14 02:35:09 +0800

    added new mysql schemas

--- backend/database/schema.sql
+++ backend/database/schema.sql
@@ -9,6 +9,19 @@ CREATE TABLE grocery_list (
     list_id INT PRIMARY KEY AUTO_INCREMENT,
     user_id INT NOT NULL,
     list_name VARCHAR(100) NOT NULL,
+    latest_item VARCHAR(100) NULL,
+    latest_item_quantity INT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (user_id) REFERENCES user(user_id)
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES user(user_id) ON DELETE CASCADE
+);
+
+CREATE TABLE todo_item (
+    item_id INT PRIMARY KEY AUTO_INCREMENT,
+    list_id INT NOT NULL,
+    product_name VARCHAR(100) NOT NULL,
+    product_quantity DECIMAL(10,2) NOT NULL,
+    is_completed BOOLEAN DEFAULT FALSE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (list_id) REFERENCES grocery_list(list_id) ON DELETE CASCADE
 );
