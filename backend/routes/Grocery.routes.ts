e16922c3d7163cf1bf4d5a03ed49e694416b5ea9
Okimchi <basilio.miko123@gmail.com>
2025-11-14 02:39:50 +0800

    added 3 new grocery routes

--- backend/routes/Grocery.routes.ts
+++ backend/routes/Grocery.routes.ts
@@ -1,9 +1,15 @@
-import express from 'express';
+import express, {RequestHandler} from 'express';
 import { authenticateToken } from '../middleware';
-import { getGroceryList } from '../Controller/Grocery.controller';
+import { getGroceryList, addGroceryList, getItems, clickItem } from '../Controller/Grocery.controller';
 
 const router = express.Router();
 
-router.get('/',authenticateToken, getGroceryList);
+router.get('/', authenticateToken, getGroceryList);
+
+router.post('/add-list', authenticateToken, addGroceryList);
+
+router.post('/fetch-Items', authenticateToken, getItems);
+
+router.put('/click-Item', authenticateToken, clickItem);
 
 export default router;
