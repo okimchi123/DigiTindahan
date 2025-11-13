b02f01141b76078bab78085c1469c40c1635f902
Okimchi <basilio.miko123@gmail.com>
2025-11-14 02:41:45 +0800

    created an api for click/update item

---
+++ DT/src/Hooks/GroceryListAPI/ClickItem.ts
@@ -0,0 +1,23 @@
+import { useMutation } from '@tanstack/react-query';
+import { axiosInstance } from '../../Model/AxiosInstance';
+
+export interface promiseType {
+  message: string;
+}
+
+interface formType{
+    value: boolean;
+    item_id: number;
+    list_id: number;
+}
+
+const fetchClickItem = async (form: formType): Promise<promiseType[]> => {
+    const { data } = await axiosInstance.put('/grocery/click-Item', form);
+    return data;
+  }
+
+const useClickItem = () => {
+ return useMutation({mutationFn: fetchClickItem});
+};
+
+export default useClickItem;
