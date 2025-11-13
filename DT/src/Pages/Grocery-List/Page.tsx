edf70b7cbed5512f0e9c8ad8c5abd012bf78e987
Okimchi <basilio.miko123@gmail.com>
2025-11-14 02:42:50 +0800

    created an addlist api inside the page

--- DT/src/Pages/Grocery-List/Page.tsx
+++ DT/src/Pages/Grocery-List/Page.tsx
@@ -1,31 +1,56 @@
 import { Plus } from "lucide-react";
 import Lists from "../../Components/Grocery-list/Lists";
 import AnimatedPage from "../../Components/UI/Animated-Container";
-import Item from "../../Components/Grocery-list/CreateItem";
 import { useState } from "react";
+import { createPortal } from "react-dom";
+import GroceryItem from "../../Components/Grocery-list/GroceryItem";
+import { useMutation } from "@tanstack/react-query";
+import { axiosInstance } from "../../Model/AxiosInstance";
+
+interface promiseType {
+  list_id: number;
+}
+
+const fetchAddList = async (): Promise<promiseType> => {
+  const { data } = await axiosInstance.post("/grocery/add-list");
+  return data;
+};
 
 export default function GroceryList() {
-  const [modal, setModal] = useState(false);
   const [isAdd, setIsAdd] = useState(false);
-  
+  const [newListId, setNewListId] = useState(0);
+
+  const { mutate } = useMutation({
+    mutationFn: fetchAddList,
+    onSuccess: (data) => {
+      setNewListId(data.list_id);
+      setIsAdd(true);
+    },
+    onError: (error)=>{
+      console.error(error);
+    }
+  });
+
   const onAdd = () => {
-    setModal(true);
-    setIsAdd(true);
-  }
-  
+    mutate();
+  };
+
   return (
     <>
-    { modal && <Item isAdd={isAdd} onClose={()=>setModal(false)} />}
-    <AnimatedPage className="container">
-      <div className="header-cont gap-4">
-        <h1 className="header-font">Grocery list</h1>
-        <button onClick={onAdd} className="p-3 bg-primary rounded-full">
-          <Plus size="26" color="white" />
-        </button>
-      </div>
-      <Lists />
-    </AnimatedPage>
+      {isAdd &&
+        createPortal(
+          <GroceryItem onClose={() => setIsAdd(false)} listId={newListId} />,
+          document.getElementById("mainPage")!
+        )}
+      <AnimatedPage className="container">
+        <div className="header-cont gap-4">
+          <h1 className="header-font">Grocery list</h1>
+          <button onClick={onAdd} className="p-3 bg-primary rounded-full">
+            <Plus size="26" color="white" />
+          </button>
+        </div>
+        <Lists />
+      </AnimatedPage>
     </>
-    
   );
 }
