27dac06393194a1d580f9d75ff9f941204e7a7d3
Okimchi <151455386+okimchi123@users.noreply.github.com>
2025-11-17 01:04:26 +0800

    added delete lists route

--- backend/routes/Grocery.routes.ts
+++ backend/routes/Grocery.routes.ts
@@ -1,6 +1,6 @@
 import express, {RequestHandler} from 'express';
 import { authenticateToken } from '../middleware';
-import { getGroceryList, addGroceryList, getItems, clickItem, AddItem } from '../Controller/Grocery.controller';
+import { getGroceryList, addGroceryList, getItems, clickItem, AddItem, deleteLists } from '../Controller/Grocery.controller';
 
 const router = express.Router();
 
@@ -12,6 +12,8 @@ router.post('/fetch-items', authenticateToken, getItems);
 
 router.put('/click-item', authenticateToken, clickItem);
 
-router.post('/add-item', authenticateToken, AddItem)
+router.post('/add-item', authenticateToken, AddItem);
+
+router.delete('/delete-lists', authenticateToken, deleteLists);
 
 export default router;
