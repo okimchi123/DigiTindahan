00d123c4445a1163aafcef36d9ed6a5d57100b2c
Okimchi <151455386+okimchi123@users.noreply.github.com>
2025-11-24 23:56:29 +0800

    initalized customer credit page

---
+++ DT/src/Pages/Customer-List/Page.tsx
@@ -0,0 +1,11 @@
+import AnimatedPage from "../../Components/UI/Animated-Container";
+
+export default function CustomerList() {
+    return(
+    <AnimatedPage>
+        <div className="header-cont">
+            <h1 className="header-font">Customer Credit</h1>
+        </div>
+    </AnimatedPage>
+    )
+}
