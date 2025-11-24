0a3be90901172e54fd17360c922cf0335cec45c5
Okimchi <151455386+okimchi123@users.noreply.github.com>
2025-11-25 03:54:34 +0800

    updated the fetchfinishedlists api every item update

--- DT/src/Hooks/GroceryListAPI/ClickItem.ts
+++ DT/src/Hooks/GroceryListAPI/ClickItem.ts
@@ -1,23 +1,29 @@
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { axiosInstance } from '../../Model/AxiosInstance';
 
 export interface promiseType {
   message: string;
 }
 
-interface formType{
-    value: boolean;
-    item_id: number;
-    list_id: number;
+interface formType {
+  value: boolean;
+  item_id: number;
+  list_id: number;
 }
 
 const fetchClickItem = async (form: formType): Promise<promiseType[]> => {
-    const { data } = await axiosInstance.put('/grocery/click-item', form);
-    return data;
-  }
+  const { data } = await axiosInstance.put('/grocery/click-item', form);
+  return data;
+}
 
 const useClickItem = () => {
- return useMutation({mutationFn: fetchClickItem});
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: fetchClickItem, onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['finished-lists'] });
+    }
+  });
 };
 
 export default useClickItem;
