f0ad6dddb0432eff3d0b8547b350a0aa3391f0a1
Okimchi <basilio.miko123@gmail.com>
2025-11-27 03:02:45 +0800

    updated ui

--- DT/src/Pages/Customer-List/Page.tsx
+++ DT/src/Pages/Customer-List/Page.tsx
@@ -1,11 +1,26 @@
 import AnimatedPage from "../../Components/UI/Animated-Container";
 import ListHeader from "../../Components/UI/List-Header";
 import { Contact } from "lucide-react";
+import { UserPlus } from "lucide-react";
+import Lists from "../../Components/Customer-List/Lists";
+import AddUser from "../../Components/Customer-List/Add-user";
+import { useState } from "react";
 
 export default function CustomerList() {
-    return(
-    <AnimatedPage>
-        <ListHeader title="Customer Credit" addFunc={()=>console.log("add")} Icon={Contact}/>
-    </AnimatedPage>
-    )
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <>
+      {isOpen && <AddUser isOpen={isOpen} onExit={() => setIsOpen(false)} />}
+      <AnimatedPage className="container">
+        <ListHeader
+          title="Customer Credit"
+          addFunc={() => setIsOpen(true)}
+          Icon={Contact}
+          AddIcon={UserPlus}
+        />
+        <Lists />
+      </AnimatedPage>
+    </>
+  );
 }
