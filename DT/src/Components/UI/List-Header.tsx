3f26f5a6cc98ff6fdce35705bc79f7928c939b07
Okimchi <basilio.miko123@gmail.com>
2025-11-27 02:10:17 +0800

    made the add btn dynamic

--- DT/src/Components/UI/List-Header.tsx
+++ DT/src/Components/UI/List-Header.tsx
@@ -1,19 +1,19 @@
-import { Plus } from "lucide-react"
 import type { LucideIcon } from "lucide-react";
 
 interface propType{
     title: string;
     addFunc: () => void;
     Icon: LucideIcon;
+    AddIcon: LucideIcon;
 }
 
-const ListHeader: React.FC<propType> = ({ title, addFunc, Icon}) => {
+const ListHeader: React.FC<propType> = ({ title, addFunc, Icon, AddIcon}) => {
 
     return(
         <div className="header-cont gap-4">
           <h1 className="header-font flex gap-2 items-center"> <Icon size='34'/> {title}</h1>
           <button onClick={addFunc} className="p-3 bg-primary rounded-full">
-            <Plus size="26" color="white" />
+            <AddIcon size="26" color="white" />
           </button>
         </div>
     )
