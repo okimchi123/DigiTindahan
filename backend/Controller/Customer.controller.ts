d1df17e35305da8655e6882612281bcb49520c5b
Okimchi <basilio.miko123@gmail.com>
2025-11-28 01:21:57 +0800

    added new api addcustomer

--- backend/Controller/Customer.controller.ts
+++ backend/Controller/Customer.controller.ts
@@ -10,8 +10,21 @@ const getCustomerList = async (req: Request, res: Response) => {
     );
     res.status(200).json(rows);
   } catch (error) {
-    res.status(500).json({ message: "Server error" });
+    res.status(500).json({ message: "Fetching customer list error" });
   }
 };
 
-export {getCustomerList};
+const addCustomer = async (req: Request, res: Response) => {
+    try {
+     const {customer_name} = req.body;
+     const userId = req.user?.userId;
+
+    await pool.query("INSERT INTO customer_credit (user_id, customer_name) values (?, ?)",[userId, customer_name]);
+
+    res.status(200).json({message:`Added ${customer_name} successfully`});
+    } catch (error) {
+    res.status(500).json({ message: "Error in adding customer" });
+    }
+}
+
+export {getCustomerList, addCustomer};
