eded7785fc50b66fe1454d920e2d34798196c4d3
Okimchi <basilio.miko123@gmail.com>
2025-11-28 00:13:48 +0800

    integrated the fetch customer api to frontend

--- DT/src/Components/Customer-List/Lists.tsx
+++ DT/src/Components/Customer-List/Lists.tsx
@@ -1,25 +1,33 @@
-import { ChevronRight} from "lucide-react";
+import { ChevronRight } from "lucide-react";
 import CustomSearch from "../../Components/UI/Search";
+import useCustomerList from "../../Hooks/CustomerAPI/FetchCustomer";
+import type { CustomerType } from "../../Hooks/CustomerAPI/FetchCustomer";
 
 const Lists: React.FC = () => {
-    return(
-        <section className="w-[80%] flex flex-col gap-2">
-        <CustomSearch />
+  const { data } = useCustomerList();
+
+  return (
+    <section className="w-[80%] flex flex-col gap-2">
+      <CustomSearch />
+      {data?.length ? (
         <ul className="flex flex-col gap-4">
-          <li className="relative">
-            <button
-              aria-haspopup="dialog"
-              className="relative bg-white shadow-[0_3px_10px_rgb(0,0,0,0.2)] select-none w-full flex flex-col items-start justify-center h-22 pl-4 rounded-xl"
-            >
-              <h2 className="font-bold text-2xl">
-                    Miko
-              </h2>
-              <ChevronRight size="22" className="absolute right-1" />
-            </button>
-          </li>
+          {data.map((customer: CustomerType) => (
+            <li className="relative">
+              <button
+                aria-haspopup="dialog"
+                className="relative bg-white shadow-[0_3px_10px_rgb(0,0,0,0.2)] select-none w-full flex flex-col items-start justify-center h-22 pl-4 rounded-xl"
+              >
+                <h2 className="font-bold text-2xl">{customer.customer_name}</h2>
+                <ChevronRight size="22" className="absolute right-1" />
+              </button>
+            </li>
+          ))}
         </ul>
-      </section>
-    )
-}
+      ) : (
+        <></>
+      )}
+    </section>
+  );
+};
 
 export default Lists;
