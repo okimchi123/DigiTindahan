b3cb32d54bd682d63db33a750a5581ef167f2db2
Okimchi <basilio.miko123@gmail.com>
2025-11-29 02:23:03 +0800

    added fetch customer credit route

--- backend/routes/Customer.routes.ts
+++ backend/routes/Customer.routes.ts
@@ -1,11 +1,13 @@
 import express, { RequestHandler } from 'express';
 import { authenticateToken } from '../middleware';
-import { getCustomerList, addCustomer } from '../Controller/Customer.controller';
+import { getCustomerList, addCustomer, getCredits } from '../Controller/Customer.controller';
 
 const router = express.Router();
 
 router.get('/', authenticateToken, getCustomerList);
 
 router.post('/add-customer', authenticateToken, addCustomer);
+
+router.post('/get-credits', authenticateToken, getCredits);
 
 export default router;
