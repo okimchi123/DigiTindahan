31b205d924e76eda4d5ede8c6eec5e1a71e2b801
Okimchi <basilio.miko123@gmail.com>
2025-11-30 02:12:59 +0800

    added the addcredit route

--- backend/routes/Customer.routes.ts
+++ backend/routes/Customer.routes.ts
@@ -1,6 +1,6 @@
 import express, { RequestHandler } from 'express';
 import { authenticateToken } from '../middleware';
-import { getCustomerList, addCustomer, getCredits } from '../Controller/Customer.controller';
+import { getCustomerList, addCustomer, getCredits, AddCredit } from '../Controller/Customer.controller';
 
 const router = express.Router();
 
@@ -9,5 +9,7 @@ router.get('/', authenticateToken, getCustomerList);
 router.post('/add-customer', authenticateToken, addCustomer);
 
 router.post('/get-credits', authenticateToken, getCredits);
+
+router.post('/add-credit', authenticateToken, AddCredit);
 
 export default router;
