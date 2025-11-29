8b25f13ca92e840dd059e89004f50106e3b9c38b
Okimchi <basilio.miko123@gmail.com>
2025-11-29 23:30:50 +0800

    updated ui

--- DT/src/Components/UI/List-Header.tsx
+++ DT/src/Components/UI/List-Header.tsx
@@ -11,7 +11,7 @@ const ListHeader: React.FC<propType> = ({ title, addFunc, Icon, AddIcon}) => {
 
     return(
         <div className="header-cont gap-4">
-          <h1 className="header-font flex gap-2 items-center"> <Icon size='34'/> {title}</h1>
+          <h1 className="header-font flex gap-2 items-center"> <Icon size='34' className="text-primary"/> {title}</h1>
           <button onClick={addFunc} className="p-3 bg-primary rounded-full">
             <AddIcon size="26" color="white" />
           </button>
